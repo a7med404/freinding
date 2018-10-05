1e8432197a3750d9d1a0412355aade0b5109b0e5
AbdilFattahBoshi <AbdilFattahBoshi001997@gmail.com>
2018-10-05 10:25:59 +0300

    Add Post table model and migration

---
+++ Modules/Posts/Database/Migrations/2018_10_04_175306_create_posts_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id',false,true);
+            $table->text('text')->nullable();
+            $table->text('links')->nullable();
+            $table->enum('type', ['text', 'picture', 'video']);
+            $table->enum('dir', ['rtl', 'ltr']);
+            $table->timestamps();
+            $table->softDeletes('deleted_at');
+            $table->engine = 'InnoDB';
+            $table->charset = 'utf8';
+            $table->collation = 'utf8_general_ci';
+            $table->foreign('user_id')->references('id')->on('users')->onUpdate('cascade')->onDelete('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
